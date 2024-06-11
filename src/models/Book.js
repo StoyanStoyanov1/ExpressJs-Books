61be3ceba9ea1284780c29e4be19c38828f6ec62
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-06-11 19:00:40 +0200

    update book Model

--- src/models/Book.js
+++ src/models/Book.js
@@ -4,10 +4,22 @@ const bookSchema = mongoose.Schema({
 	title: {
 		type: String,
 		required: true,
+		validate: {
+			validator: (value) => {
+				return value.length > 2;
+			},
+			message: 'Title muss be at least 2 characters.',
+		},
 	},
 	author: {
 		type: String,
 		required: true,
+		validate: {
+			validator: (value) => {
+				return value.length > 2;
+			},
+			message: 'Title muss be at least 2 characters.',
+		},
 	},
 	image: {
 		type: String,
@@ -16,6 +28,12 @@ const bookSchema = mongoose.Schema({
 	review: {
 		type: String,
 		required: true,
+		validate: {
+			validator: (value) => {
+				return value.length > 10;
+			},
+			message: 'Review muss be at least 10 characters.',
+		},
 	},
 	genre: {
 		type: String,
@@ -24,6 +42,12 @@ const bookSchema = mongoose.Schema({
 	stars: {
 		type: Number,
 		required: true,
+		validate: {
+			validator: (value) => {
+				return value >= 1 && value <= 5;
+			},
+			message: 'Stars Must Be Between 1 And 5'
+		},
 	},
 	wishingList: [{
 		type: mongoose.Schema.Types.ObjectId,
