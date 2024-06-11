1e144c2567d250318da4367af1007e58d9a73a4e
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-06-11 17:10:14 +0200

    404

--- src/router.js
+++ src/router.js
@@ -3,9 +3,14 @@ const router = require('express').Router();
 const homeController = require('./controlller/homeController');
 const authController = require('./controlller/authController');
 const userController = require('./controlller/bookController');
+const {getErrorMessage} = require("./utils/errorUtils");
 
 router.use('/', homeController);
 router.use('/auth', authController);
 router.use('/book', userController);
+
+router.use('*', (req, res) => {
+	res.render('404', {error: 'Not Found'});
+})
 
 module.exports = router;
